2176013e6566b5c71c009116ef1ae8685f0aa4c1
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-11-07 09:15:46 +0700

    initial hrm front end

--- simple_meta/src/main/java/com/axonactive/simple_meta/configuration/WebsocketConfiguration.java
+++ simple_meta/src/main/java/com/axonactive/simple_meta/configuration/WebsocketConfiguration.java
@@ -13,7 +13,7 @@ public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("http://localhost:8080")
                 .withSockJS();
     }
 
