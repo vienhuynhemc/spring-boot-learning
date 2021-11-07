ccef3aab5e8f1a2e6975e2558a9b23f9bd5182ac
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-11-07 16:09:50 +0700

    add lottie and tooltip in material

--- hrm-front-end/src/app/app.module.ts
+++ hrm-front-end/src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Lottie
+import player from 'lottie-web';
+import { LottieModule } from 'ngx-lottie';
+export function playerFactory() {
+  return player;
+}
+// thư viện get request
+import { HttpClientModule } from '@angular/common/http';
+// tooltip
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // http
+    HttpClientModule,
+    // lottie
+    LottieModule.forRoot({ player: playerFactory }),
+    // tooltip
+    MatTooltipModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
